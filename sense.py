8f3c3e0a5f2724249e3d7236c6330462b4574511
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2020-03-28 14:30:35 +0100

    Compute colors, pressure & temp views

--- sense.py
+++ sense.py
@@ -1,69 +1,113 @@
 #!/usr/bin/env python
 
-from abc import ABC, abstractmethod
-from sense_emu import SenseHat
+from sense_hat import SenseHat
 
-class View(ABC):
-    @abstractmethod
-    def draw(self, hat: SenseHat):
-        pass
+E = 0.1
+MIN_TEMP = -40
+MAX_TEMP = abs(MIN_TEMP)
+MIN_PRESSURE = 950
+MAX_PRESSURE = 1050
+        
+class View:
+  """View interface"""
+  def draw(self, hat):
+    pass
+
+class ColorCalc:
+  """Computes color for value from given range"""
+  def __init__(self, min_value, max_value):
+    self.min_value = min_value
+    self.levels = max_value - min_value
+    self.prev_value = None
+    self.color = None
+
+  def compute_color(self, value):
+    if (
+      self.prev_value is not None
+        and self.color is not None
+        and abs(self.prev_value - value) < E
+      ):
+      return self.color
+      
+    self.prev_value = value
+      
+    score = 0
+    if value > self.min_value:
+      score = min(1.0, (abs(self.min_value - value) / self.levels))
+        
+    saturation = 255 * (score * 2 - 1) if score > 0.5 else 255 * (1 - score * 2)
+    saturation = int(saturation)
+        
+    self.color = (saturation, 0, 0) if score > 0.5 else (0, 0, saturation)
+        
+    return self.color
+
 
 class TemperatureView(View):
-    def draw(self, hat: SenseHat):
-        temp = hat.temp
-        #coeffitient = abs(temp) / (if temp < 0 30 else 105)
-            
-        print(temp)
+  def __init__(self):
+    self.level = ColorCalc(MIN_TEMP, MAX_TEMP)
+        
+  def draw(self, hat):
+    temp = hat.temp
+    color = self.level.compute_color(temp)
+    pixels = [color for _ in range(64)]
+
+    hat.set_pixels(pixels)
 
 class PressureView(View):
-    def draw(self, hat: SenseHat):
-        print("test2")
-
-class ContextViewer:
-    def __init__(self, hat):
-        self.hat = hat
-        self.views = []
-        self.current_view = None
-        self.current_idx = None
+  def __init__(self):
+    self.level = ColorCalc(MIN_PRESSURE, MAX_PRESSURE)
         
-    def register_view(self, view):
-        view_type = type(view)
-        if not issubclass(view_type, View):
-            raise TypeError("Not a View subclass; got {}".format(view_type))
+  def draw(self, hat):
+    pressure = hat.pressure
+    color = self.level.compute_color(pressure)
+    pixels = [color for _ in range(64)]
 
-        self.views.append(view)
+    hat.set_pixels(pixels)
 
-    def handle_event(self, event):
-        if event.action == 'pressed':
-            new_idx = self.current_idx
-            new_idx += { 'left': -1, 'right': +1 }.get(event.direction, 0)
+class EventHandler:
+  def __init__(self, hat):
+      self.hat = hat
+      self.views = []
+      self.current_view = None
+      self.current_idx = None
+      
+  def register_view(self, view):
+    view_type = type(view)
+    if not issubclass(view_type, View):
+      raise TypeError("Not a View subclass; got {}".format(view_type))
 
-            max_idx = len(self.views) - 1
-            if new_idx > max_idx:
-                new_idx = 0
-            elif new_idx < 0:
-                new_idx = max_idx
+    self.views.append(view)
 
-            self.current_idx = new_idx
-            self.hat.clear()
-            self.current_view = self.views[self.current_idx]
+  def handle_event(self, event):
+    if event.action == 'pressed':
+      new_idx = self.current_idx
+      new_idx += { 'left': -1, 'right': +1 }.get(event.direction, 0)
+
+      max_idx = len(self.views) - 1
+      if new_idx > max_idx:
+        new_idx = 0
+      elif new_idx < 0:
+        new_idx = max_idx
+
+      self.current_idx = new_idx
+      self.hat.clear()
+      self.current_view = self.views[self.current_idx]
         
+  def event_loop(self):
+    self.current_idx = 0
+    self.current_view = self.views[self.current_idx]
+    while True:
+      for event in self.hat.stick.get_events():
+        self.handle_event(event)
+      
+      self.current_view.draw(self.hat)
 
-    def event_loop(self):
-        self.current_idx = 0
-        self.current_view = self.views[self.current_idx]
-        while True:
-            for event in self.hat.stick.get_events():
-                self.handle_event(event)
-            
-            self.current_view.draw(self.hat)
-
-
-
+# Program begins here
 hat = SenseHat()
-
 hat.clear()
-context_viewer = ContextViewer(hat)
-context_viewer.register_view(TemperatureView())
-context_viewer.register_view(PressureView())
-context_viewer.event_loop()
+
+event_handler = EventHandler(hat)
+event_handler.register_view(TemperatureView())
+event_handler.register_view(PressureView())
+event_handler.event_loop()
