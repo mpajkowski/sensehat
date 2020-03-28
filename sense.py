21091d459b315835c8230cbdac03f32114b92e73
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2020-03-28 17:35:38 +0100

    View indicator

--- sense.py
+++ sense.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from sense_hat import SenseHat
+import copy
 
 E = 0.1
 MIN_TEMP = -40
@@ -9,11 +10,23 @@ MIN_PRESSURE = 950
 MAX_PRESSURE = 1050
 MIN_HUMIDITY = 0
 MAX_HUMIDITY = 100
-        
+MIN_DEGREE = 0
+MAX_DEGREE = 360
+WHITE = (0xFF, 0xFF, 0xFF)
+BLACK = (0x00, 0x00, 0x00)
+
+def set_pixel(hat, idx, color):
+  x = idx % 8
+  y = int(idx / 8)
+
+  hat.set_pixel(x, y, color)
+  
+
 class View:
   """View interface"""
   def draw(self, hat):
-    pass
+    raise NotImplementedError
+
 
 class ColorCalc:
   """Computes color for value from given range"""
@@ -30,18 +43,18 @@ class ColorCalc:
         and abs(self.prev_value - value) < E
       ):
       return self.color
-      
+
     self.prev_value = value
-      
+
     score = 0
     if value > self.min_value:
       score = min(1.0, (abs(self.min_value - value) / self.levels))
-        
+
     saturation = 255 * (score * 2 - 1) if score > 0.5 else 255 * (1 - score * 2)
     saturation = int(saturation)
-        
+
     self.color = (saturation, 0, 0) if score > 0.5 else (0, 0, saturation)
-        
+
     return self.color
 
 
@@ -52,32 +65,60 @@ class TemperatureView(View):
   def draw(self, hat):
     temp = hat.temp
     color = self.color_calc.compute(temp)
-    pixels = [color for _ in range(64)]
-
-    hat.set_pixels(pixels)
+    
+    for idx in range(48):
+      set_pixel(hat, idx, color)
 
 class PressureView(View):
   def __init__(self, color_calc):
     self.color_calc = color_calc
-        
+
   def draw(self, hat):
     pressure = hat.pressure
     color = self.color_calc.compute(pressure)
-    pixels = [color for _ in range(64)]
-
-    hat.set_pixels(pixels)
     
+    for idx in range(48):
+      set_pixel(hat, idx, color)
+
+
+class OrientationView(View):
+  def __init__(self, color_calc_roll, color_calc_pitch, color_calc_yaw):
+    self.color_calc_roll = color_calc_roll
+    self.color_calc_pitch = color_calc_pitch
+    self.color_calc_yaw = color_calc_yaw
+
+  def draw(self, hat):
+    orientation = hat.orientation
+
+    roll = orientation.get('roll', 0)
+    pitch = orientation.get('pitch', 0)
+    yaw = orientation.get('yaw', 0)
+    
+    color_roll = self.color_calc_roll.compute(roll)
+    color_pitch = self.color_calc_pitch.compute(pitch)
+    color_yaw = self.color_calc_yaw.compute(yaw)
+
+    for idx in range(16):
+      set_pixel(hat, idx, color_roll)
+      
+    for idx in range(16, 32):
+      set_pixel(hat, idx, color_pitch)
+      
+    for idx in range(32, 48):
+      set_pixel(hat, idx, color_yaw)
+
+
 class HumidityView(View):
   def __init__(self, color_calc):
     self.color_calc = color_calc
-        
+
   def draw(self, hat):
     humidity = hat.humidity
     color = self.color_calc.compute(humidity)
-    pixels = [color for _ in range(64)]
 
-    hat.set_pixels(pixels)
-  
+    for idx in range(48):
+      set_pixel(hat, idx, color)
+
 
 class EventHandler:
   def __init__(self, hat):
@@ -85,7 +126,7 @@ class EventHandler:
       self.views = []
       self.current_view = None
       self.current_idx = None
-      
+
   def register_view(self, view):
     view_type = type(view)
     if not issubclass(view_type, View):
@@ -97,32 +138,49 @@ class EventHandler:
     if event.action == 'pressed':
       new_idx = self.current_idx
       new_idx += { 'left': -1, 'right': +1 }.get(event.direction, 0)
-
-      max_idx = len(self.views) - 1
-      if new_idx > max_idx:
-        new_idx = 0
-      elif new_idx < 0:
-        new_idx = max_idx
-
-      self.current_idx = new_idx
-      self.hat.clear()
-      self.current_view = self.views[self.current_idx]
+      
+      if new_idx != self.current_idx:
+        max_idx = len(self.views) - 1
+        if new_idx > max_idx:
+          new_idx = 0
+        elif new_idx < 0:
+          new_idx = max_idx
+          
+        # turn off current idx
+        set_pixel(self.hat, 48 + self.current_idx, BLACK)
         
+        # turn on new
+        set_pixel(self.hat, 48 + new_idx, WHITE)
+  
+        self.current_idx = new_idx
+        self.current_view = self.views[self.current_idx]
+
   def event_loop(self):
     self.current_idx = 0
+    set_pixel(self.hat, 48, WHITE)
     self.current_view = self.views[self.current_idx]
     while True:
       for event in self.hat.stick.get_events():
         self.handle_event(event)
-      
+
       self.current_view.draw(self.hat)
 
 # Program begins here
 hat = SenseHat()
+hat.set_imu_config(True, True, True)
 hat.clear()
 
 event_handler = EventHandler(hat)
+
 event_handler.register_view(TemperatureView(ColorCalc(MIN_TEMP, MAX_TEMP)))
 event_handler.register_view(PressureView(ColorCalc(MIN_PRESSURE, MAX_PRESSURE)))
 event_handler.register_view(HumidityView(ColorCalc(MIN_HUMIDITY, MAX_HUMIDITY)))
+
+calc_roll = ColorCalc(MIN_DEGREE, MAX_DEGREE)
+calc_pitch = ColorCalc(MIN_DEGREE, MAX_DEGREE)
+calc_yaw = ColorCalc(MIN_DEGREE, MAX_DEGREE)
+orientation_view = OrientationView(calc_roll, calc_pitch, calc_yaw)
+
+event_handler.register_view(orientation_view)
+
 event_handler.event_loop()
